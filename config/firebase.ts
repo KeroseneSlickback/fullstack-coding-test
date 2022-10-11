25194e9f565cd2fef152d173112bbc572bf5bbae
Will <mitchellspaur@gmail.com>
2022-10-11 23:23:57 +0900

    removed non-working commented API keys

--- config/firebase.ts
+++ config/firebase.ts
@@ -12,16 +12,6 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAU51HwUO3dM6g2cVtURNwPX2_hlQ_w4JY",
-//   authDomain: "coding-test-72fa5.firebaseapp.com",
-//   projectId: "coding-test-72fa5",
-//   storageBucket: "coding-test-72fa5.appspot.com",
-//   messagingSenderId: "155227620151",
-//   appId: "1:155227620151:web:f3f15c046d232477c110b2",
-//   measurementId: "G-49XW5E9YQV",
-// };
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth();
